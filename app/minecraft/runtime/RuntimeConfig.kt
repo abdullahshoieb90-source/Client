6c97450c0ab67b2e2d34f8aa8e8a1a237547ca24
abdullahshoieb90-source <abdullahshoieb90@gmail.com>
2026-08-04 08:01:42 +0300

    Add RuntimeConfig data class for Minecraft runtime

---
+++ app/minecraft/runtime/RuntimeConfig.kt
@@ -0,0 +1,27 @@
+package com.bedrock.client.runtime
+
+import java.io.File
+
+data class RuntimeConfig(
+
+    /** Minecraft package name */
+    val packageName: String,
+
+    /** Path to the installed APK */
+    val apkPath: String,
+
+    /** Native libraries directory */
+    val nativeLibraryDir: File,
+
+    /** Client files directory */
+    val filesDir: File,
+
+    /** Client cache directory */
+    val cacheDir: File,
+
+    /** Sandbox directory */
+    val sandboxDir: File,
+
+    /** Game data directory inside sandbox */
+    val gameDataDir: File
+)
